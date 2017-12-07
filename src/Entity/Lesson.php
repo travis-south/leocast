5fb11cf082320dafb5366b1f48d5b36f27874d15
Irvin Capagcuan <irvin@capagcuan.org>
2017-12-07 15:56:22 +0800

    Initial entity.

---
+++ src/Entity/Lesson.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Lessons
+ *
+ * @ApiResource
+ * @ORM\Entity
+ */
+class Lesson
+{
+    // ...
+}
---
+++ src/Entity/Series.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Series
+ *
+ * @ApiResource
+ * @ORM\Entity
+ */
+class Series
+{
+    // ...
+}
